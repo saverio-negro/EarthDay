2256a28f45167415ee112c75a08e38ef20f1300f
Saverio Negro <saverionegro@gmail.com>
2025-04-22 18:46:34 +0200

    Initial Commit

---
+++ EarthDay/EarthDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthDayApp.swift
+//  EarthDay
+//
+//  Created by Saverio Negro on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct EarthDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
